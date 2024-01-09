d406dc6af64d94f29bcc5ac08ff5f895824fc22c
Aikagra-2212 <f20200294@pilani.bits-pilani.ac.in>
2024-01-09 15:26:28 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+let name= "Harry";
+function App() {
+  return (
+    <h1>
+      Native Token Balance: 1000 <br/>
+      Change % in last 12 hours: 12%
+    </h1>
+  );
+}
+
+export default App;
